b99eae7b91b5955fe2be40d9dad5ed539e01cde9
phucvo <phucvoaff@gmail.com>
2023-06-10 01:02:21 +0700

    added personal aws for testing

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,5 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = 'iud9mo89mi';
+const apiId = 'ge5li4oql9';
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`;
 
 export const authConfig = {
